79c01e14963362796372c97346200a51b5113e73
Sammie Adams <sammie.adams112@gmail.com>
2026-08-09 21:56:29 -0700

    updated js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const menuButton = document.querySelector(".fa-bars");
 
 const navLinks = document.querySelector(".nav__links");
+
+menuButton.addEventListener("click", function () {
+  navLinks.classList.toggle("menuOpen");
+});
